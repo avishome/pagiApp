8ef99ecf64623c12f9b1258838fddd3b92e09d41
avishome <avishome@gmail.com>
2020-11-04 21:34:15 +0200

    1 file modified

--- src/app/adepter/instructions/instructions.component.ts
+++ src/app/adepter/instructions/instructions.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit , Input} from '@angular/core';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import { ToolbarService, LinkService, ImageService, HtmlEditorService, TableService, RichTextEditorComponent } from '@syncfusion/ej2-angular-richtexteditor';
 
-import { CKEditorComponent, CKEditor5 } from '@ckeditor/ckeditor5-angular';
 
 
 @Component({
@@ -13,7 +12,7 @@ import { CKEditorComponent, CKEditor5 } from '@ckeditor/ckeditor5-angular';
 export class InstructionsComponent implements OnInit {
   @Input() data;
   @Input() grade;
-  public Editor = ClassicEditor;
+
   public conf = {
         toolbar: [ ]
     }
