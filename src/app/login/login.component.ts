bd125641630c0420e77e75c79ad62d5389c3b065
avishome <avishome@gmail.com>
2020-11-10 13:17:47 +0200

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ServerConnectService } from '../server-connect.service';
-import {Login} from '../login';
-import { CookieService } from 'ngx-cookie-service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ServerConnectService } from "../server-connect.service";
+import { Login } from "../login";
+import { CookieService } from "ngx-cookie-service";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-
 export class LoginComponent implements OnInit {
   public echo = "";
-  constructor(private router:Router, private server: ServerConnectService, private cookieService: CookieService) { }
-  nextpage(){
-    this.router.navigate(['/selectWorkPage']);
+  constructor(
+    private router: Router,
+    private server: ServerConnectService,
+    private cookieService: CookieService
+  ) {}
+  nextpage() {
+    this.router.navigate(["/selectWorkPage"]);
   }
-  loginF(user,pass){
-  this.echo = "Verifies details...";
-  this.server.login(user.value,pass.value).subscribe(
-    (data: Login)=>{
-      
-      console.log(data);
-      if(data.secsess){
+  loginF(user, pass) {
+    this.echo = "Verifies details...";
+    this.server.login(user.value, pass.value).subscribe((data: Login) => {
+      if (data.secsess) {
         this.echo = "You redirect";
-        this.cookieService.set("Token",data.token);
+        this.cookieService.set("Token", data.token);
         this.nextpage();
-      }
-      else this.echo = "Login Fail";
-      }
-    )
+      } else this.echo = "Login Fail";
+    });
   }
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
